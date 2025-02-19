27975ed3b57704f7f8eb587c1c3f2a2068033628
luiggy <luiggylml@gmail.com>
2025-02-18 21:03:20 -0500

    init project microservices

---
+++ src/main/java/com/codigo/msregister/MsRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.codigo.msregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codigo/msregister/MsRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigo.msregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
